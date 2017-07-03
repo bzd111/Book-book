0bd78f781486697d63a11a70271b67cc99e29aef
bzd111 <1240450484@qq.com>
2017-07-03 13:23:57 +0800

    change url of the books

--- config.py
+++ config.py
@@ -2,13 +2,13 @@
 # -*- coding: utf-8 -*-
 
 
-DA_URL = "http://www.biquge.cc/html/60/60821/"
-SHENG_URL = "http://www.biquge.cc/html/156/156129/"
-YI_URL = "http://www.biquge.cc/html/9/9378/"
+# DA_URL = "http://www.biquge.cc/html/60/60821/"
+# SHENG_URL = "http://www.biquge.cc/html/156/156129/"
+# YI_URL = "http://www.biquge.cc/html/9/9378/"
 
-# DA_URL = "http://www.qu.la/book/176/"
-# SHENG_URL = "http://www.qu.la/book/24868/"
-# YI_URL = "http://www.qu.la/book/16431/"
+DA_URL = "http://www.qu.la/book/176/"
+SHENG_URL = "http://www.qu.la/book/24868/"
+YI_URL = "http://www.qu.la/book/16431/"
 
 
 REDIS_DB = 0
