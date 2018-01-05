a4807f5b363d3fe1574d892b8c9568295ffd1948
zhangcheng <zhangcheng@uyunsoft.cn>
2018-01-05 09:30:31 +0800

    [Mod] modify code for checking

--- celery.py
+++ celery.py
@@ -2,7 +2,7 @@ from __future__ import absolute_import
 import os
 from celery import Celery
 
-DIRNAME = os.path.basename(os.path.dirname(os.path.abspath("__file__")))
+DIRNAME = os.path.basename(os.path.dirname(os.path.abspath(__file__)))
 app = Celery(DIRNAME, include=['{}.tasks'.format(DIRNAME)])
 
 app.config_from_object('{}.celeryconfig'.format(DIRNAME))
