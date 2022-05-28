056cdf30189e4dfaa4e52b94a250e192a85445c2
dingtalk_psddtx <>
2022-05-28 15:51:12 +0800

    use env replace local_setting
    Signed-off-by: dingtalk_psddtx <>

--- config.py
+++ config.py
@@ -3,7 +3,10 @@
 import logging.config
 import os
 import sys
+import json
 from pathlib import Path
+
+from yarl import URL
 
 ROOT_PATH = Path.cwd()
 LOGS_DIR = ROOT_PATH / 'logs'
@@ -11,16 +14,14 @@ JSON_FILE = ROOT_PATH / 'urls.json'
 
 DATA_DB = os.path.join(ROOT_PATH, 'fake_useragent.json')
 
-# SHENG_URL = 'https://www.biquge.com.cn/book/23488/'
-# DIAN_URL = 'https://www.biquge.com.cn/book/31583/'
-# YUAN_URL = 'https://www.biquge.com.cn/book/15517/'
-# SAN_URL = 'https://www.biquge.com.cn/book/31833/'
-# TIAN_URL = 'https://biquge.com.cn/book/32101/'
-# LONG_URL = 'https://www.biquge.com.cn/book/36825/'
-
 URLS_DICT = {
-    # "https://www.xbiquge.so/book/55233/":"择日飞升",
+    "https://www.xbiquge.so/book/55233/":"择日飞升",
 }
+
+URLS_DICT = os.getenv("URLS_DICT", URLS_DICT)
+
+if type(URLS_DICT) == str:
+    URLS_DICT = json.loads(URLS_DICT)
 
 SLEEP_TIME = 10
 
@@ -94,10 +95,12 @@ makedirs(LOGS_DIR)
 logging.config.dictConfig(LOGGING)
 
 # read from ENV
-mail_to_list = ['zxc@bzd111.me']
+mail_to_list = os.getenv('MAIL_TO_LIST', ['zxc@bzd111.me'])
+if type(mail_to_list) == str:
+    mail_to_list = json.loads(mail_to_list)
 mail_host = 'smtp.qq.com'
 mail_user = os.getenv('MAIL_USER')
-mail_pass = os.getenv('MailPass')
+mail_pass = os.getenv('MAIL_PASS')
 mail_postfix = 'qq.com'
 mail_port = 465
 
